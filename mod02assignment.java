86cdde206226442ed27a743a36bbf64c366c7a8d
ConnnorMartineau <53840256+ConnnorMartineau@users.noreply.github.com>
2019-10-07 22:08:43 -0500

    Add files via upload

---
+++ mod02assignment.java
@@ -0,0 +1,15 @@
+
+public class mod02assignment {
+
+	public static void main(String[] args) {
+		
+		childClass childClassObject = new childClass();
+		int [] a = new int [] {10, 7, 8, 2, 1};
+
+		childClassObject.displayArray1(a);
+		childClassObject.sortArray1(a);
+		childClassObject.addArray1(a);
+	}
+
+}
+
